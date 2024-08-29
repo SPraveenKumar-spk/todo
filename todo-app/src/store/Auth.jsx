0c1bd19afea8605593b0a1c6e638cc7adab4c039
S. Praveen Kumar <109852619+SPraveenKumar-spk@users.noreply.github.com>
2024-08-29 09:38:01 +0530

    Update Auth.jsx

--- todo-app/src/store/Auth.jsx
+++ todo-app/src/store/Auth.jsx
@@ -15,7 +15,7 @@ export const AuthProvider = ({ children }) => {
     localStorage.removeItem("token");
   };
 
-  const baseURL = "https://todo-qehc.onrender.com";
+  const baseURL = "https://todo-tawny-ten.vercel.app";
 
   let isLoggedIn = !!token;
 
